de5756b2916ab890115fc397244228722f82941d
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2020-11-25 22:28:32 +0900

    -0と-oを追加

--- main/line_selector.py
+++ main/line_selector.py
@@ -7,20 +7,23 @@ sys.stdout.reconfigure(line_buffering=True)
 
 p = argparse.ArgumentParser()
 p.add_argument('target_file')
+p.add_argument('-0', '--zero', action='store_true')
+p.add_argument('-o', '--output', action='store_true')
 args = p.parse_args()
 
 try:
     stdin_lines = sys.stdin.readlines()
-    # query
-    print(stdin_lines.pop(0), end='')
-    # key
-    print(stdin_lines.pop(0), end='')
+    if args.output:
+        # query
+        print(stdin_lines.pop(0), end='')
+        # key
+        print(stdin_lines.pop(0), end='')
     index_queue = [int(line.split('\t')[0]) for line in stdin_lines]
     index_set = set(index_queue)
     max_index = max(index_set)
     lines = {}
     with open(args.target_file, 'r') as f:
-        i = 0
+        i = 0 if not args.zero else -1
         while len(index_queue) > 0:
             index = index_queue.pop(0)
             if index in lines:
