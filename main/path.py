ee82b5c57a8822c1bf71cc7eacb72c9ca066679d
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2021-01-10 10:03:34 +0900

    --tilde_homeオプションを追加

--- main/path.py
+++ main/path.py
@@ -6,8 +6,17 @@ import argparse
 sys.stdout.reconfigure(line_buffering=True)
 
 p = argparse.ArgumentParser()
-p.add_argument('-p', '--path', default='auto', help='auto|absolute|relative (auto)')
-p.add_argument('--updir_depth', default='3', help='auto時に絶対パスと相対パスを切り替える深さ (3)')
+p.add_argument('-p',
+               '--path',
+               default='auto',
+               help='auto|absolute|relative (auto)')
+p.add_argument('--updir_depth',
+               default='3',
+               help='auto時に絶対パスと相対パスを切り替える深さ (3)')
+p.add_argument('-t',
+               '--tilde_home',
+               action='store_true',
+               help='ホームディレクトリを~にする (False)')
 args = p.parse_args()
 
 
@@ -15,12 +24,16 @@ def transform(line):
     slash = line.endswith('/') if line != '/' else False
     if args.path == 'absolute':
         line = os.path.abspath(line)
+        if args.tilde_home and line.startswith(os.environ['HOME']):
+            line = '~{}'.format(line[len(os.environ['HOME']):])
     elif args.path == 'relative':
         line = os.path.relpath(line)
     else:
         line = os.path.relpath(line)
         if line.startswith('../' * int(args.updir_depth)):
             line = os.path.abspath(line)
+            if args.tilde_home and line.startswith(os.environ['HOME']):
+                line = '~{}'.format(line[len(os.environ['HOME']):])
     if slash:
         line += '/'
     return line
