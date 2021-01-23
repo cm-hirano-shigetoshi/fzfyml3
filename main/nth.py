fadb4f916579ef278f3ebb800e14c13acb39d25b
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2021-01-23 21:14:18 +0900

    リファクタリング

--- main/nth.py
+++ main/nth.py
@@ -14,71 +14,44 @@ args = p.parse_args()
 
 
 def filter_with_range(line, r, d):
-    if d is None:
-        if '..' not in r:
-            # 単一要素指定
+    if '..' not in r:
+        # 単一要素指定
+        r = int(r) - 1 if int(r) > 0 else int(r)
+        if d is None:
             islands = re.findall(r'\S+\s*', line)
-            r = int(r) - 1 if int(r) > 0 else int(r)
             return islands[r]
         else:
-            # 範囲指定
-            def get_range_text(line, start, end):
-                islands = re.findall(r'\S+\s*', line)
-                start = int(start) - 1 if int(start) > 0 else int(start)
-                if int(end) == -1:
-                    return ''.join(islands[start:])
-                else:
-                    end = int(end) if int(end) > 0 else int(end) + 1
-                    return ''.join(islands[start:end])
-
-            if r == '..':
-                start = 1
-                end = -1
-                return get_range_text(line, start, end)
-            elif r.startswith('..'):
-                start = 1
-                end = r[2:]
-                return get_range_text(line, start, end)
-            elif r.endswith('..'):
-                start = r[:-2]
-                end = -1
-                return get_range_text(line, start, end)
-            else:
-                start = r[:r.find('.')]
-                end = r[r.find('.') + 2:]
-                return get_range_text(line, start, end)
-    else:
-        if '..' not in r:
-            # 単一要素指定
-            r = int(r) - 1 if int(r) > 0 else int(r)
             return line.split(d)[r]
-        else:
-            # 範囲指定
-            def get_range_text(line, start, end):
-                islands = line.split(d)
-                start = int(start) - 1 if int(start) > 0 else int(start)
-                if int(end) == -1:
-                    return d.join(islands[start:])
-                else:
-                    end = int(end) if int(end) > 0 else int(end) + 1
-                    return d.join(islands[start:end])
-
-            if r == '..':
-                start = 1
-                end = -1
-                return get_range_text(line, start, end)
-            elif r.startswith('..'):
-                start = 1
-                end = r[2:]
-                return get_range_text(line, start, end)
-            elif r.endswith('..'):
-                start = r[:-2]
-                end = -1
-                return get_range_text(line, start, end)
+    else:
+        # 範囲指定
+        def get_range_text(line, start, end):
+            if d is None:
+                islands = re.findall(r'\S+\s*', line)
             else:
-                start = r[:r.find('.')]
-                end = r[r.find('.') + 2:]
-                return get_range_text(line, start, end)
+                islands = line.split(d)
+            start = int(start) - 1 if int(start) > 0 else int(start)
+            if int(end) == -1:
+                return ('' if d is None else d).join(islands[start:])
+            else:
+                end = int(end) if int(end) > 0 else int(end) + 1
+                return ('' if d is None else d).join(islands[start:end])
+
+        if r == '..':
+            start = 1
+            end = -1
+            return get_range_text(line, start, end)
+        elif r.startswith('..'):
+            start = 1
+            end = r[2:]
+            return get_range_text(line, start, end)
+        elif r.endswith('..'):
+            start = r[:-2]
+            end = -1
+            return get_range_text(line, start, end)
+        else:
+            start = r[:r.find('.')]
+            end = r[r.find('.') + 2:]
+            return get_range_text(line, start, end)
 
 
 def some_transform(line):
