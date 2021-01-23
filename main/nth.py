d4aa273a1539c6e1890c69fc75addae758c06a62
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2021-01-23 15:50:49 +0900

    nth.pyにデリミタ指定を追加

--- main/nth.py
+++ main/nth.py
@@ -13,56 +13,84 @@ p.add_argument('-p', '--plus', action='store_true')
 args = p.parse_args()
 
 
-def filter_with_range(line, r):
-    line = line.lstrip()
-    if '..' not in r:
-        # 単一要素指定
-        islands = re.findall(r'\S+\s*', line)
-        r = int(r) - 1 if int(r) > 0 else int(r)
-        return islands[r]
-    else:
-        # 範囲指定
-        def get_range_text(line, start, end):
+def filter_with_range(line, r, d):
+    if d is None:
+        if '..' not in r:
+            # 単一要素指定
             islands = re.findall(r'\S+\s*', line)
-            start = int(start) - 1 if int(start) > 0 else int(start)
-            if int(end) == -1:
-                return ''.join(islands[start:])
-            else:
-                end = int(end) if int(end) > 0 else int(end) + 1
-                return ''.join(islands[start:end])
-
-        if r == '..':
-            start = 1
-            end = -1
-            return get_range_text(line, start, end)
-        elif r.startswith('..'):
-            start = 1
-            end = r[2:]
-            return get_range_text(line, start, end)
-        elif r.endswith('..'):
-            start = r[:-2]
-            end = -1
-            return get_range_text(line, start, end)
+            r = int(r) - 1 if int(r) > 0 else int(r)
+            return islands[r]
         else:
-            start = r[:r.find('.')]
-            end = r[r.find('.') + 2:]
-            return get_range_text(line, start, end)
+            # 範囲指定
+            def get_range_text(line, start, end):
+                islands = re.findall(r'\S+\s*', line)
+                start = int(start) - 1 if int(start) > 0 else int(start)
+                if int(end) == -1:
+                    return ''.join(islands[start:])
+                else:
+                    end = int(end) if int(end) > 0 else int(end) + 1
+                    return ''.join(islands[start:end])
+
+            if r == '..':
+                start = 1
+                end = -1
+                return get_range_text(line, start, end)
+            elif r.startswith('..'):
+                start = 1
+                end = r[2:]
+                return get_range_text(line, start, end)
+            elif r.endswith('..'):
+                start = r[:-2]
+                end = -1
+                return get_range_text(line, start, end)
+            else:
+                start = r[:r.find('.')]
+                end = r[r.find('.') + 2:]
+                return get_range_text(line, start, end)
+    else:
+        if '..' not in r:
+            # 単一要素指定
+            r = int(r) - 1 if int(r) > 0 else int(r)
+            return line.split(d)[r]
+        else:
+            # 範囲指定
+            def get_range_text(line, start, end):
+                islands = line.split(d)
+                start = int(start) - 1 if int(start) > 0 else int(start)
+                if int(end) == -1:
+                    return d.join(islands[start:])
+                else:
+                    end = int(end) if int(end) > 0 else int(end) + 1
+                    return d.join(islands[start:end])
+
+            if r == '..':
+                start = 1
+                end = -1
+                return get_range_text(line, start, end)
+            elif r.startswith('..'):
+                start = 1
+                end = r[2:]
+                return get_range_text(line, start, end)
+            elif r.endswith('..'):
+                start = r[:-2]
+                end = -1
+                return get_range_text(line, start, end)
+            else:
+                start = r[:r.find('.')]
+                end = r[r.find('.') + 2:]
+                return get_range_text(line, start, end)
 
 
 def some_transform(line):
-    if args.delimiter is None:
-        if args.nth == '':
-            return line
-        ranges = args.nth.split(',')
-        parts = []
-        for r in ranges:
-            result = filter_with_range(line, r)
-            parts.append(result)
-        output = ''.join(parts).rstrip()
-        return output
-    else:
-        pass
-    return line
+    if args.nth == '':
+        return line
+    ranges = args.nth.split(',')
+    parts = []
+    for r in ranges:
+        result = filter_with_range(line, r, args.delimiter)
+        parts.append(result)
+    output = ''.join(parts).lstrip().rstrip()
+    return output
 
 
 try:
