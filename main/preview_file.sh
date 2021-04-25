9ccf485ccd1d6dd0241dd3c3c666bbde2a34ce37
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2021-04-25 22:10:24 +0900

    JSONでもプレビューする

--- main/preview_file.sh
+++ main/preview_file.sh
@@ -7,6 +7,8 @@ elif file -b $1 | grep -q 'directory'; then
     ls -l --color $1/
 elif file -b $1 | grep -q 'executable'; then
     xxd $1
+elif file -b $1 | grep -q 'JSON data'; then
+    cat $1
 else
     file -b $1
 fi
